f26644206ea867eb71b75c4464458a7380620086
adamkong1995 <adamkong256@gmail.com>
2019-04-07 16:21:04 +0800

    fixed typo

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // Decide what environment and return relevant keys
 if (process.env.NODE_ENV === 'production') {
-     modules.exports = require('./prod');
+     module.exports = require('./prod');
 } else {
-    modules.exports = require('./dev');
+    module.exports = require('./dev');
 }
