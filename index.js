84e4cdb01638cb19a845ac2cf648cf7e8a9cbd4a
adamkong1995 <adamkong256@gmail.com>
2019-04-07 15:14:36 +0800

    setup passport js and google oauth strategy

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// Passport js config
+app.use(
+    cookieSession({
+        // cookie will be store for 30 days
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
 require('./routes/authRoutes')(app); 
-
-
 
 // dynamic port for heroku production environment
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT); 
